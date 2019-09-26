6e5315771d54996be79aa2ad8f802ec33c9d82c4
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-26 18:23:55 +0000

    refactored and tests complete

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,15 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
     if (error) {
-      return callback(error);
+      return callback(error, null);
     }
     try {
       const data = JSON.parse(body);
-      const desc = data[0].description;
+      const info = data[0].description;
+      const desc = info.substring(0, info.length - 1);
       return callback(null, desc);
     } catch (error) {
-      return callback(error);
+      return callback(error, null);
     }
   });
 };
